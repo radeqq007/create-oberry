592c16de7bd367ff71e1a3ba65fe999df92a94b1
radeqq007 <radeqq007@interia.pl>
2026-05-07 19:37:48 +0200

    feat: add typescript template

---
+++ templates/TypeScript/src/main.ts
@@ -0,0 +1,24 @@
+import type { Ref } from "oberry";
+import { $, $effect, $ref } from "oberry";
+
+const count: Ref<number> = $ref(0);
+const counter = $("#counter-value");
+counter.bind(count);
+
+$effect(() => {
+  if (count() > 0) {
+    counter.css({ color: "var(--secondary)" });
+  } else if (count() < 0) {
+    counter.css({ color: "var(--primary)" });
+  } else {
+    counter.css({ color: "var(--foreground)" });
+  }
+});
+
+$("#increment-btn").on("click", () => {
+  count(count() + 1);
+});
+
+$("#decrement-btn").on("click", () => {
+  count(count() - 1);
+});
